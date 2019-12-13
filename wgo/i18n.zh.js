5d85bb888c197cdfc6dd9d0ecdad8e0a1bfbeaf6
yzyray <turnon130>
2019-12-13 08:24:46 +0800

    修改关于

--- wgo/i18n.zh.js
+++ wgo/i18n.zh.js
@@ -5,7 +5,7 @@ WGo.lang = "zh";
 WGo.i18n[WGo.lang] = {
     "about-text": "<h1>Lizzie Player 0.1</h1>"
         + "<p>Lizzie Player 扩展自<a href='http://wgo.waltheri.net/player' target='_blank'>WGo.js</a>, 用HTML5编写,支持电脑/移动端浏览Lizzie分析后的棋谱,包括胜率图,查找恶手等功能,同时支持查看本地棋谱.</p>"
-        + "<p>你所查看的棋谱来自Lizzie Yzy版,免费公开(<a href='https://pan.baidu.com/s/1q615GHD62F92mNZbTYfcxA' target='_blank'>点击下载</a>),在Lizzie中分析后可以使用分享功能分享到此网页以供其他电脑,手机,pad等查看</p>"
+        + "<p>你所查看的棋谱来自Lizzie Yzy版,免费公开(<a href='https://pan.baidu.com/s/1q615GHD62F92mNZbTYfcxA' target='_blank'>点击下载</a>),在Lizzie中分析后可以使用分享功能分享到此网页,然后在其他终端中查看</p>"
         //+ "<p></p>"
         + "<p>版权 &copy; 2019 Yzy</p>",
     "black": "黑方",
